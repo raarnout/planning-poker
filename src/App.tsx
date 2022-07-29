ada8eb7c383a195e64fec1e0bcddeb5e0dcf9827
Ronald <ronaldaarnoutse@gmail.com>
2022-07-29 19:38:14 +0200

    connect to firebase database and print values from database

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from './persistence/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
-function App() {
+const App = () => {
+  const [games, setGames] = useState<{id: string, title: string;}[]>([]);
+  const gamesCollectionRef = collection(db, "games");
+
+  useEffect(() => {
+    const getGames = async () => {
+      const data = await getDocs(gamesCollectionRef);
+      console.log(data);
+      console.log(data.docs)
+      setGames(data.docs.map((doc) => ({ title: doc.data().title, id: doc.id })));
+    }
+
+    getGames();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <body>
-        hello world
-      </body>
+      <h1>planning-poker</h1>
+      {games.map((game) => {
+        return (<div><h2>Title: {game.title}</h2></div>)
+      })}
     </div>
   );
 }
