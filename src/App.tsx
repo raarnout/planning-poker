8b6cdbba7b863bce4d322dc5ea30fbcb40b2de32
Ronald <ronaldaarnoutse@gmail.com>
2022-08-02 15:01:59 +0200

    run eslint en prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,47 @@
-import { useState, useEffect } from "react";
-import "./scss/styles.scss";
-import { db } from "./persistence/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from 'react';
+import './scss/styles.scss';
+import { db } from './persistence/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 interface IGame {
-  id: string;
-  title: string;
+	id: string;
+	title: string;
 }
 
 const getGames = async () => {
-  const gamesCollectionRef = collection(db, "games");
-  const data = await getDocs(gamesCollectionRef);
-  const newData = data.docs.map((doc) => ({
-    id: doc.id,
-    title: doc.data().title,
-  }));
-  return newData;
+	const gamesCollectionRef = collection(db, 'games');
+	const data = await getDocs(gamesCollectionRef);
+	const newData = data.docs.map(doc => ({
+		id: doc.id,
+		title: doc.data().title
+	}));
+	return newData;
 };
 
 const App = () => {
-  const [games, setGames] = useState<IGame[]>([]);
+	const [games, setGames] = useState<IGame[]>([]);
 
-  useEffect(() => {
-    getGames()
-      .then((games) => {
-        setGames(games);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+	useEffect(() => {
+		getGames()
+			.then(games => {
+				setGames(games);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, []);
 
-  return (
-    <div className="App">
-      {games.map((game) => {
-        return (
-          <div key={game.id}>
-            <h2>Title: {game.title}</h2>
-          </div>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="App">
+			{games.map(game => {
+				return (
+					<div key={game.id}>
+						<h2>Title: {game.title}</h2>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default App;
