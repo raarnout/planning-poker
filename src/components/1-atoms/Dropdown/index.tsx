b1fb35897aff0623c375922bf990569d55668048
Ronald <ronaldaarnoutse@gmail.com>
2022-10-10 09:52:53 +0200

    dirty push 2

--- src/components/1-atoms/Dropdown/index.tsx
+++ src/components/1-atoms/Dropdown/index.tsx
@@ -1,36 +1,31 @@
-import { ChangeEventHandler } from 'react';
-
 export interface DropDownProps {
-    label: string;
-    options: Array<Options>;
+	label: string;
+	options: Array<Options>;
+	onChange: () => void;
 }
 
 export type Options = {
-    text: string;
-    key: string;
-    value: string;
-}
-
-const onChange = (event: ChangeEventHandler<HTMLSelectElement>) => {
-    console.log('dorpdown value changed');
-}
+	text: string;
+	key: string;
+	value: string;
+};
 
 /**
  * Primary UI component for user interaction
  */
-const DropDown = ({
-    label, options
-}: DropDownProps) => {
-    return (
-        <label>
-            {label}
-            <select value={label} onChange={onChange}>
-                {options.map(option => (
-                    <option value={option.value} key={option.key}>{option.text}</option>
-                ))}
-            </select>
-        </label>
-    );
+const DropDown = ({ label, options }: DropDownProps) => {
+	return (
+		<label>
+			{label}
+			<select value={label} onChange={onChange}>
+				{options.map(option => (
+					<option value={option.value} key={option.key}>
+						{option.text}
+					</option>
+				))}
+			</select>
+		</label>
+	);
 };
 
 export default DropDown;
--- src/components/5-pages/New/index.tsx
+++ src/components/5-pages/New/index.tsx
@@ -6,74 +6,79 @@ import { createUser } from '../../../api/users';
 import { useNavigate } from 'react-router-dom';
 
 export const New = () => {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState('');
-    const [username, setUsername] = useState('');
-    const [disabled, setDisabled] = useState(true);
+	const navigate = useNavigate();
+	const [title, setTitle] = useState('');
+	const [username, setUsername] = useState('');
+	const [disabled, setDisabled] = useState(true);
 
-    const handleChangeTitle = (event: FormEvent<HTMLInputElement>) => {
-        event.preventDefault();
-        setTitle(event.currentTarget.value);
-    };
+	const handleChangeTitle = (event: FormEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		setTitle(event.currentTarget.value);
+	};
 
-    const handleChangeUsername = (event: FormEvent<HTMLInputElement>) => {
-        event.preventDefault();
-        setUsername(event.currentTarget.value);
-    };
+	const handleChangeUsername = (event: FormEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		setUsername(event.currentTarget.value);
+	};
 
-    const create = async () => {
-        const userId: string = await createUser({
-            name: username,
-            role: 'HOST'
-        });
-        console.log(userId);
-        const users = [userId]
-        const gameId: string = await createGame('games', { title, users });
-        navigate(`/${gameId}`);
-    };
+	const handleDropdownChanged = (event: FormEvent<HTMLSelectElement>) => {
+		console.log('handleDropdownChanged', event);
+	};
 
-    const votingOptions: Array<Options> = [
-        { text: '1,2,3,5,8', key: 'one', value: 'one' },
-        { text: '1,2,4,8,16', key: 'two', value: 'two' },
-        { text: 'xs,s,m,l,xl', key: 'three', value: 'three' }
-    ];
+	const create = async () => {
+		const userId: string = await createUser({
+			name: username,
+			role: 'HOST'
+		});
+		console.log(userId);
+		const users = [userId];
+		const gameId: string = await createGame('games', { title, users });
+		navigate(`/${gameId}`);
+	};
 
-    useEffect(() => {
-        setDisabled(title === '' || username === '');
-    }, [title, username])
+	const votingOptions: Array<Options> = [
+		{ text: '1,2,3,5,8', key: 'one', value: 'one' },
+		{ text: '1,2,4,8,16', key: 'two', value: 'two' },
+		{ text: 'xs,s,m,l,xl', key: 'three', value: 'three' }
+	];
 
-    return (
-        <Fragment>
-            <div>
-                <div>
-                    <span>Title</span>
-                    <input
-                        placeholder="Title..."
-                        value={title}
-                        onChange={handleChangeTitle}
-                    ></input>
-                </div>
-                <div>
-                    <span>Username</span>
-                    <input
-                        placeholder="Username..."
-                        value={username}
-                        onChange={handleChangeUsername}
-                    ></input>
-                </div>
-                <div>
-                    <DropDown
-                        label='Voting System'
-                        options={votingOptions}
-                    ></DropDown>
-                </div>
-            </div>
-            <Button
-                label={'Create Game'}
-                type={ButtonType.SUBMIT}
-                onClick={create}
-                disabled={disabled}
-            ></Button>
-        </Fragment>
-    );
+	useEffect(() => {
+		setDisabled(title === '' || username === '');
+	}, [title, username]);
+
+	return (
+		<Fragment>
+			<div>
+				<div>
+					<span>Title</span>
+					<input
+						placeholder="Title..."
+						value={title}
+						onChange={handleChangeTitle}
+					></input>
+				</div>
+				<div>
+					<span>Username</span>
+					<input
+						placeholder="Username..."
+						value={username}
+						onChange={handleChangeUsername}
+					></input>
+				</div>
+				<div>
+					<DropDown
+						label="Voting System"
+						options={votingOptions}
+						onChange={handleDropdownChanged}
+					></DropDown>
+				</div>
+			</div>
+			<Button
+				label={'Create Game'}
+				type={ButtonType.SUBMIT}
+				onClick={create}
+				disabled={disabled}
+			></Button>
+		</Fragment>
+	);
 };
